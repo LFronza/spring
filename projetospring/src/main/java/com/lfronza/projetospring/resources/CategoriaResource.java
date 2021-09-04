d271d7a448a723e2928573ad51571b8fc9e7bc4b
Leonardo Fronza <lfronza312@gmail.com>
2021-09-04 19:03:07 -0300

    Testando o REST

---
+++ projetospring/src/main/java/com/lfronza/projetospring/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.lfronza.projetospring.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "REST OK!";
+	}
+	
+}
