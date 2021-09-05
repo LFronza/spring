4830aec535719516cfbda801e175518e8b5acaf3
Leonardo Fronza <lfronza312@gmail.com>
2021-09-04 22:00:21 -0300

    Criando Service e Repository para a Categoria

---
+++ projetospring/src/main/java/com/lfronza/projetospring/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.lfronza.projetospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lfronza.projetospring.domain.Categoria;
+import com.lfronza.projetospring.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
