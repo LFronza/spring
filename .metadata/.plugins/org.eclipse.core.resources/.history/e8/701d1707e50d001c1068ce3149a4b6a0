090e6bf2237c2a195947989e4ff7ad3d9b592c2c
Leonardo Fronza <lfronza312@gmail.com>
2021-09-05 13:59:33 -0300

    produtos e associações + correção de bugs do banco de dados h2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e8/701d1707e50d001c1068ce3149a4b6a0
@@ -0,0 +1,13 @@
+package com.lfronza.projetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringApplication.class, args);
+	}
+
+}
--- projetospring/src/main/java/com/lfronza/projetospring/domain/Categoria.java
+++ projetospring/src/main/java/com/lfronza/projetospring/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.lfronza.projetospring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -17,6 +20,10 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 	}
@@ -43,6 +50,14 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -59,6 +74,7 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 }
